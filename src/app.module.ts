c453333e43a2d4e22ab2ce0289c9f095016b2094
Gabriel Carvalho Misael <91130049+GabrielMisaelC@users.noreply.github.com>
2025-06-26 13:23:29 -0300

    [feat] create resource post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { PostModule } from './post/post.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [StudentModule, TeacherModule],
+  imports: [StudentModule, TeacherModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
