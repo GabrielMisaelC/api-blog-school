3da179e95d22ec73e78958f5b92ad4b04921000c
Gabriel Carvalho Misael <91130049+GabrielMisaelC@users.noreply.github.com>
2025-06-26 13:22:51 -0300

    [feat] create resource teacher

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [StudentModule],
+  imports: [StudentModule, TeacherModule],
   controllers: [AppController],
   providers: [AppService],
 })
