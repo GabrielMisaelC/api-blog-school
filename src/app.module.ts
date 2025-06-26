b9796149f050880323cd57abfad56b2345e5447c
Gabriel Carvalho Misael <91130049+GabrielMisaelC@users.noreply.github.com>
2025-06-26 13:19:12 -0300

    [feat] create resource student

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentModule } from './student/student.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
