c8d460c9b3f2ea0938ec1185f9d3ed18fe68232b
Gabriel Carvalho Misael <91130049+GabrielMisaelC@users.noreply.github.com>
2025-06-26 14:08:24 -0300

    [Feat] prisma database implementation

---
+++ prisma/migrations/20250626165158_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "Teacher" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "schoolMaterial" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN DEFAULT false,
+    "teacherid" INTEGER,
+    CONSTRAINT "Post_teacherid_fkey" FOREIGN KEY ("teacherid") REFERENCES "Teacher" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN DEFAULT false,
+    "postid" INTEGER,
+    "studentid" INTEGER,
+    CONSTRAINT "Comment_postid_fkey" FOREIGN KEY ("postid") REFERENCES "Post" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Comment_studentid_fkey" FOREIGN KEY ("studentid") REFERENCES "Student" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
