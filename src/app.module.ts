2f9240480b21304bfad7167822e0a4aee4502e47
Gabriel Terra <gabrielterra07@gmail.com>
2025-07-06 22:58:47 -0300

    Criação do método GET /posts/search

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
 import { PersonModule } from './person/person.module';
 import { SchoolMaterialModule } from './school-material/school-material.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [PostModule, CommentModule, PersonModule, SchoolMaterialModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
